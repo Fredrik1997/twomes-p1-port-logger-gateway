853ddfeccdd8c49b0523ed2a5b6875594bcc37f9
Fredrik <f.o.lautenbag@gmail.com>
2020-11-04 21:13:50 +0100

    added wifi/thingspeak connection, switches between WiFi and ESP-NOW succesful but development needed

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,51 @@
 #include <esp_now.h>
 #include <WiFi.h>
 #include <Arduino.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <utils.h>
 
-#define macArrayLength 6
+#define macArrayLength 6      //number of position in a mac address
 
-#define testDevices 6
-#define receiveInterval 1005
+#define testDevices 6         //maximum nodes
+
+#define sendInterval 30000    //sendinterval thingspeak 30 seconds
 
 byte lastAddedNode = 0; //last node which registered at this gateway
 
-typedef struct esp_now_message
+unsigned long lastTimeSended = 0;   //to save what time (relative to millis) the last message to thingspeak was sent.
+
+const char *ssid = userWiFiSSID;        //ssid internet access point, configure in utils.h
+const char *password = userWiFipassword;//password internet access point, configure in utils.h
+
+const char *serverName = userServerName; // Domain Name with full URL Path for HTTP POST Request, configure in utils.h
+String apiKey = userAPIkey;              // Service API Key, configure in utils.h
+
+typedef struct esp_now_message    //this is how data from a node is saved
 {
   byte macID[macArrayLength];
   int temperature1;
   int temperature2;
 } esp_now_message;
 
-esp_now_message node[testDevices];
+esp_now_message node[testDevices];  //allocate memory for 'testDevices'
 
 // esp_now_message node0, node1, node2, node3, node4, node5;
 
 // esp_now_message *nodes[testDevices] = {&node0, &node1, &node2, &node3, &node4, &node5};
 
-struct communicationTests
+struct communicationTests       //save this parameters for statics
 {
   unsigned int missedCalls;
   unsigned long lastContact;
   unsigned long firstRegisterTime;
   unsigned long averageInterval;
+  unsigned long totalCalls;
 };
 
 struct communicationTests nodeTests[testDevices];
 
-typedef struct struct_message
+typedef struct struct_message   //this is how data is sended, REVIEW: this is possible on a different way
 {
   int temperature1;
   int temperature2;
@@ -43,6 +56,7 @@ struct_message myData;
 //prototype functions
 byte checkExistingMac(byte *pntToMac);
 void putMac(byte *pntToMac, byte selectedRow);
+boolean ESPnowconfig(boolean requestedState);
 
 // callback function that will be executed when data is received
 void OnDataRecv(const uint8_t *mac_addr, const uint8_t *incomingData, int len)
@@ -77,10 +91,10 @@ void OnDataRecv(const uint8_t *mac_addr, const uint8_t *incomingData, int len)
     if (myData.temperature1 != (node[nodeNumber].temperature1 + 1)) //if we missed a call
     {
       int difference = myData.temperature1 - node[nodeNumber].temperature1;
-      #define thresholdDifference 60
-      if (difference > thresholdDifference || difference < thresholdDifference)
+#define thresholdDifference 60
+      if ((difference > thresholdDifference) || ((difference * -1) > thresholdDifference))
       {
-        Serial.printf("Differene = %i", difference);
+        Serial.printf("Differene = %i\n", difference);
         Serial.println("difference to big, so not saved");
       }
       else
@@ -93,7 +107,8 @@ void OnDataRecv(const uint8_t *mac_addr, const uint8_t *incomingData, int len)
     {
       long interval = logTime - nodeTests[nodeNumber].lastContact;
       nodeTests[nodeNumber].averageInterval += interval;
-      nodeTests[nodeNumber].averageInterval = nodeTests[nodeNumber].averageInterval / 2;    //average not calculated on a right way
+      nodeTests[nodeNumber].averageInterval = nodeTests[nodeNumber].averageInterval / 2; //average not calculated on a right way
+      nodeTests[nodeNumber].totalCalls++;
     }
     nodeTests[nodeNumber].lastContact = logTime;
     node[nodeNumber].temperature1 = myData.temperature1;
@@ -102,6 +117,76 @@ void OnDataRecv(const uint8_t *mac_addr, const uint8_t *incomingData, int len)
   digitalWrite(LED_BUILTIN, LOW);
 }
 
+//input:  requested state of WiFi communication
+//output: true if succeed, otherwise false
+//effect: sets WiFi communication in requested state
+boolean WiFiconfig(boolean requestedState)
+{
+  if (requestedState)
+  {
+    // Serial.print("Wi-Fi Channel: ");
+    // Serial.println(WiFi.channel());
+    WiFi.begin(ssid, password, 3);
+    //Serial.println("Connecting");
+    while (WiFi.status() != WL_CONNECTED)
+    {
+      delay(1);
+      //Serial.print(".");
+    }
+    // Serial.println("");
+    // Serial.print("Connected to WiFi network with IP Address: ");
+    // Serial.println(WiFi.localIP());
+    if (WiFi.status() == WL_CONNECTED)
+    {
+      // Serial.print("Wi-Fi Channel: ");
+      // Serial.println(WiFi.channel());
+      return true;
+    }
+  }
+  else
+  {
+    if (WiFi.disconnect(true, true) == 1)
+    {
+      return true;
+    }
+  }
+  return false;
+}
+
+//input:  requested state of ESP-NOW communication
+//output: true if succeed, otherwise false
+//effect: sets ESP-NOW communication in requested state
+boolean ESPnowconfig(boolean requestedState)
+{
+  if (requestedState)
+  {
+    WiFi.softAP("bullshit", "bulllshit", 1); //REVIEW: use this to change wifi radio channel, but this should be an other function
+    WiFi.mode(WIFI_STA);                     //REVIEW: check if this is necessarly
+    if (esp_now_init() == ESP_OK)
+    {
+      if (esp_now_register_recv_cb(OnDataRecv) == ESP_OK) // Once ESPNow is successfully Init, we will register for recv CB to, get recv packer info
+      {
+        return true;
+      }
+    }
+  }
+  else
+  {
+    if (esp_now_unregister_recv_cb() == ESP_OK)
+    {
+      if (esp_now_deinit() == ESP_OK)
+      {
+        return true;
+      }
+    }
+  }
+  Serial.println("ESPNOW config fault");
+  return false;
+}
+
+//input:  pointer to mac address, byte to free row/position
+//output: nothing
+//effect: puts new device into 'database'
 void putMac(byte *pntToMac, byte selectedRow)
 {
   for (byte macTestFill = 0; macTestFill < macArrayLength; macTestFill++)
@@ -109,6 +194,62 @@ void putMac(byte *pntToMac, byte selectedRow)
     node[selectedRow].macID[macTestFill] = (*pntToMac + macTestFill);
   }
 }
+
+//input:  
+//output: false if something wrong, true if succeed
+//effect: sends defined parameters to ThingSpeak API
+boolean sendToThingSpeak()
+{
+  HTTPClient http;
+  if (!http.begin(serverName))
+  {
+    return false;
+  }
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+  String httpRequestData = "api_key=" + apiKey +
+                           "&field1=" + String(nodeTests[0].missedCalls) +
+                           "&field2=" + String(nodeTests[0].totalCalls) +
+                           "&field3=" + String(nodeTests[1].missedCalls) +
+                           "&field4=" + String(nodeTests[1].totalCalls) +
+                           "&field5=" + String(nodeTests[2].missedCalls) +
+                           "&field6=" + String(nodeTests[2].totalCalls) +
+                           "&field7=" + String(nodeTests[3].missedCalls) +
+                           "&field8=" + String(nodeTests[3].totalCalls);
+
+  int httpResponseCode = http.POST(httpRequestData);
+  // Serial.print("httpRequestData : ");
+  // Serial.println(httpRequestData);
+
+  // Serial.print("HTTP Response code: ");
+  // Serial.println(httpResponseCode);
+
+  http.end();
+  if (httpResponseCode == 200)
+  {
+    return true;
+  }
+  return false;
+}
+
+//input:  nothing
+//output: nothing
+//effect: returns statics from active nodes to serial monitor
+void printStatics()
+{
+  for (byte testCount = 0; testCount < lastAddedNode; testCount++)
+  {
+    Serial.printf("Node %i:\n", testCount);
+    Serial.printf("Last contact %lu microseconds ago\n", (millis() - nodeTests[testCount].lastContact));
+    Serial.printf("Missed calls %i\n", nodeTests[testCount].missedCalls);
+    Serial.printf("Registered %lu seconds ago\n", (((millis() - nodeTests[testCount].firstRegisterTime) / 1000) / 60));
+    Serial.printf("Average interval time is %lu microseconds\n", nodeTests[testCount].averageInterval);
+    Serial.printf("Total calls received: %lu\n", nodeTests[testCount].totalCalls);
+    Serial.printf("----\n");
+  }
+  Serial.println("-----------------------------");
+}
+
 
 //input:  pointer to mac address, byte array with 'macArrayLength' positions
 //output: 255: did not find existing node, 0-254 found existing node
@@ -143,41 +284,32 @@ void setup()
 {
   Serial.begin(115200);
   Serial.println("Serial active");
-  delay(10);
-  // for (byte macSearch = 0; macSearch < testDevices; macSearch++) //print macs from memory to monitor
-  // {
-  //   //Serial.print(node[macSearch].macID[1]);
-  //   Serial.printf("Node: %i has mac: ", macSearch);
-  //   Serial.printf("%02x:%02x:%02x:%02x:%02x:%02x\n", node[macSearch].macID[0], node[macSearch].macID[1], node[macSearch].macID[2], node[macSearch].macID[3], node[macSearch].macID[4], node[macSearch].macID[5]);
-  // }
-
-  // uint8_t macToTest[macArrayLength] = {4, 5, 6, 7, 8, 9};    //test for fucntino checkExistingMac
-  // Serial.printf("matching node : %x", checkExistingMac(macToTest));
 
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
   WiFi.mode(WIFI_STA); //Set device as a Wi-Fi Station
-
-  if (esp_now_init() != ESP_OK) //Init ESP-NOW
-  {
-    Serial.println("Error initializing ESP-NOW");
-    return;
-  }
-
-  esp_now_register_recv_cb(OnDataRecv); // Once ESPNow is successfully Init, we will register for recv CB to, get recv packer info
+  ESPnowconfig(true);
 }
 
 void loop()
 {
-  for (byte testCount = 0; testCount < lastAddedNode; testCount++)
+  if ((millis() - lastTimeSended) > sendInterval)
   {
-    Serial.printf("Node %i:\n", testCount);
-    Serial.printf("Last contact %lu microseconds ago\n", (millis() - nodeTests[testCount].lastContact));
-    Serial.printf("Missed calls %i\n", nodeTests[testCount].missedCalls);
-    Serial.printf("Registered %lu seconds ago\n", (millis() - nodeTests[testCount].firstRegisterTime) / 1000);
-    Serial.printf("Average interval time is %lu microseconds\n", nodeTests[testCount].averageInterval);
-    Serial.printf("----\n");
+    ESPnowconfig(false);
+    WiFiconfig(true); //Serial.printf("WiFiconfig = %x", WiFiconfig(true));
+    if (WiFi.status() == WL_CONNECTED)
+    {
+      if (sendToThingSpeak())
+      {
+        lastTimeSended = millis();
+      }
+    }
+    else
+    {
+      Serial.println("WiFi Disconnected");
+    }
+    //printStatics();
+    WiFiconfig(false);
+    ESPnowconfig(true);
   }
-  Serial.println("-----------------------------");
-  delay(5000);
 }
